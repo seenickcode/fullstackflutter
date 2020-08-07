3c94f8a68a7d20ef5d688ca5406081935eb056d8
Nicholas Manning <nicholas.manning@gmail.com>
2020-08-07 08:36:39 -0400

    Added starter project oauth example.

---
+++ examples/oauth/oauth/lib/oauth.dart
@@ -0,0 +1,11 @@
+/// oauth
+///
+/// A Aqueduct web server.
+library oauth;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
---
+++ examples/oauth/oauth/test/harness/app.dart
@@ -0,0 +1,32 @@
+import 'package:oauth/oauth.dart';
+import 'package:aqueduct_test/aqueduct_test.dart';
+
+export 'package:oauth/oauth.dart';
+export 'package:aqueduct_test/aqueduct_test.dart';
+export 'package:test/test.dart';
+export 'package:aqueduct/aqueduct.dart';
+
+/// A testing harness for oauth.
+///
+/// A harness for testing an aqueduct application. Example test file:
+///
+///         void main() {
+///           Harness harness = Harness()..install();
+///
+///           test("GET /path returns 200", () async {
+///             final response = await harness.agent.get("/path");
+///             expectResponse(response, 200);
+///           });
+///         }
+///
+class Harness extends TestHarness<OauthChannel> {
+  @override
+  Future onSetUp() async {
+
+  }
+
+  @override
+  Future onTearDown() async {
+
+  }
+}
