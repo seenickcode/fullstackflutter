f6207c0c0d50491906adc10e9bfe1b69189bae4e
Nick Manning <hello@manninglabs.com>
2020-09-15 09:56:21 -0400

    Final code updates to the app.

--- simple_end_to_end/03-flutter-http-client/app/lib/main.dart
+++ simple_end_to_end/03-flutter-http-client/app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   await DotEnv().load('local.env');
--- simple_end_to_end/03-flutter-http-client/app/lib/screens/home.dart
+++ simple_end_to_end/03-flutter-http-client/app/lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Home extends StatefulWidget {
   Home({Key key}) : super(key: key);
@@ -13,29 +15,32 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Example"),
-      ),
-      body: Center(
-          child: Column(
+        appBar: AppBar(
+          title: Text("Example"),
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-              RaisedButton(
-                  child: Text("Fetch Data"),
-                  color: Colors.blue,
-                  textColor: Colors.white,
-                  onPressed: handleFetchTap),
-              RaisedButton(child: Text("Reset"), onPressed: handleResetTap),
-            ]),
+                RaisedButton(
+                    child: Text("Fetch Data"),
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    onPressed: handleFetchTap),
+                RaisedButton(child: Text("Reset"), onPressed: handleResetTap),
+              ],
+            ),
             Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
               Flexible(
                   child: Text(message,
-                      maxLines: 20, style: TextStyle(fontSize: 18.0))),
-            ]),
-          ])),
-    );
+                      maxLines: 20, style: TextStyle(fontSize: 18.0)))
+            ])
+          ],
+        )));
   }
 
   handleFetchTap() async {
@@ -46,24 +51,20 @@ class _HomeState extends State<Home> {
     });
   }
 
-  handleResetTap() async {
+  handleResetTap() {
     setState(() {
       message = "";
     });
   }
 
-  static Future<String> fetch() async {
+  Future<String> fetch() async {
     try {
-      var url = '${DotEnv().env['API_BASE_URI']}/example';
-      http.Response response = await http.get(url);
-      if (response.statusCode == 200 || response.statusCode == 201) {
-        print(response.body);
-        return response.body;
-      }
+      var url = "${DotEnv().env['API_BASE_URI']}/example";
+      var resp = await get(url);
+      return resp.body;
     } catch (error) {
       print(error);
       return error.toString();
     }
-    return null;
   }
 }
