ee910d2dcaffc0b831fbc846a5dba44a573bdf7f
Nick Manning <hello@manninglabs.com>
2020-10-17 09:33:46 -0400

    Updated Dockerfile.

--- simple_end_to_end/04-deploying-server/server/bin/main.dart
+++ simple_end_to_end/04-deploying-server/server/bin/main.dart
@@ -1,21 +1,21 @@
 import 'package:server/server.dart';
 
 Future main() async {
-  print("PORT env var is ${Platform.environment["PORT"]}");
-
-  final port = int.parse(Platform.environment["PORT"]);
-
-  print("port parsed is $port");
+  final port = int.parse(Platform.environment["PORT"] ?? "8888");
 
   final app = Application<ServerChannel>()
     ..options.configurationFilePath = "config.yaml"
     ..options.port = port;
 
-  await app.startOnCurrentIsolate();
+  await app.start(numberOfInstances: 1, consoleLogging: true);
+
+  // this doesn't work for Google Cloud Run
+  // await app.startOnCurrentIsolate();
 
   print("Application started on port: ${app.options.port}.");
   print("Use Ctrl-C (SIGINT) to stop running the application.");
 
+  // needed when we want to send a SIGINT to a running Docker container
   ProcessSignal.sigint.watch().listen((signal) {
     print("receivd SIGINT, exiting");
     exit(0);
